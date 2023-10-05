14a4014cc81700c425b0baf60ed36bcfd0bbaa7f
faujiiiahhh <framandaadelia@gmail.com>
2023-10-05 09:12:25 +0700

    Hasil Modisikasi No. 4

--- Pemilihan2Percobaan110.java
+++ Pemilihan2Percobaan110.java
@@ -1,20 +1,25 @@
 import java.util.Scanner;
 
+import javax.sound.sampled.SourceDataLine;
+
 public class Pemilihan2Percobaan110{
     public static void main (String[]args){
         Scanner input10 = new Scanner (System.in);
 
         int tahun;
+
         System.out.println("Masukkan Tahun");
         tahun = input10.nextInt();
       
-        if((tahun % 4)==0){
+        if((tahun % 400)==0){
             if ((tahun % 100)!= 0)
             System.out.println("Tahun Kabisat");
-            else 
+        }else if ((tahun % 4)==0){
+                if ((tahun % 100)!=0)
+                System.out.println("Tahun Kabisat");
+                else
+                System.out.println("Bukan Tahun Kabisat");
+            }else
             System.out.println("Bukan Tahun Kabisat");
-
-        }else
-        System.out.println("Bukan Tahun Kabisat");
+        }
     }
-}
